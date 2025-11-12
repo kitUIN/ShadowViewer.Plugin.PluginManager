a9288b859ba458c4c2efed8e3ec3cea7d424e1d5
kitUIN <Kit_UIN@outlook.com>
2025-11-12 16:20:19 +0800

    Fix

---
+++ Helpers/PluginNameHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace ShadowViewer.Plugin.PluginManager.Helpers;
+
+/// <summary>
+/// 
+/// </summary>
+public static class PluginNameHelper
+{
+    /// <summary>
+    /// GetPluginId in manager page
+    /// </summary>
+    /// <param name="rawName"></param>
+    /// <returns></returns>
+    public static string GetPluginId(string rawName)
+    {
+        return rawName.Replace("ShadowViewer.Plugins.", "");
+    }
+}
