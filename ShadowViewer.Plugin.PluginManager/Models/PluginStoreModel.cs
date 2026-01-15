0291ff1f70972900ae872345ae5dc6ecb27398f7
kituin <kit_uin@outlook.com>
2026-01-16 03:47:15 +0800

    Style | Store Install Progress

--- ShadowViewer.Plugin.PluginManager/Models/PluginStoreModel.cs
+++ ShadowViewer.Plugin.PluginManager/Models/PluginStoreModel.cs
@@ -26,6 +26,8 @@ using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using ShadowPluginLoader.WinUI.Enums;
 using ShadowPluginLoader.WinUI.Models;
+using ShadowViewer.Sdk.Enums;
+using ShadowViewer.Sdk.Services;
 using Symbol = FluentIcons.Common.Symbol;
 
 namespace ShadowViewer.Plugin.PluginManager.Models;
@@ -293,141 +295,142 @@ public partial class PluginStoreModel : PluginStoreBaseModel
         DependencyCheckStatus = flag;
     }
 
-    private async Task ShowInstallContentDialog(Func<IProgress<PipelineProgress>, Task> installAction)
+
+    /// <summary>
+    /// Builds the content of the progress.
+    /// </summary>
+    private UIElement BuildProgressContent()
     {
+        var panel = new StackPanel
+        {
+            Spacing = 4,
+            Padding = new Thickness(0, 0, 0, 20),
+            Height = 70,
+            Width = 340,
+        };
+
+        // 主任务
+        var taskGrid = new Grid { ColumnSpacing = 8 };
+        taskGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(40) });
+        taskGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+        taskGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+        taskGrid.Children.Add(new TextBlock { Name = "TaskProgressValue", Text = "00.00%" });
+        taskGrid.Children.Add(new ProgressBar
+        { Name = "TaskProgress", Minimum = 0, Maximum = 1, Width = 160, Height = 8 });
+        taskGrid.Children.Add(new TextBlock
+        { Name = "TaskProgressStatus", Text = nameof(InstallPipelineStep.Feeding) });
+
+        Grid.SetColumn(taskGrid.Children[0] as FrameworkElement, 0);
+        Grid.SetColumn(taskGrid.Children[1] as FrameworkElement, 1);
+        Grid.SetColumn(taskGrid.Children[2] as FrameworkElement, 2);
+
+        // 子任务
+        var subGrid = new Grid { Name = "SubTaskGrid", ColumnSpacing = 8, Margin = new Thickness(20, 0, 0, 0) };
+        subGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(40) });
+        subGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+        subGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+        subGrid.Children.Add(new TextBlock { Name = "SubTaskProgressValue", Text = "00.00%" });
+        subGrid.Children.Add(new ProgressBar
+        { Name = "SubTaskProgress", Minimum = 0, Maximum = 1, Width = 140, Height = 8 });
+        subGrid.Children.Add(new TextBlock { Name = "SubTaskStatus", Text = "----" });
+
+        Grid.SetColumn(subGrid.Children[0] as FrameworkElement, 0);
+        Grid.SetColumn(subGrid.Children[1] as FrameworkElement, 1);
+        Grid.SetColumn(subGrid.Children[2] as FrameworkElement, 2);
+
+        panel.Children.Add(taskGrid);
+        panel.Children.Add(subGrid);
+
+        return panel;
+    }
+    /// <summary>
+    /// Notifies the information bar.
+    /// </summary>
+    private async Task NotifyInfoBar(Func<IProgress<PipelineProgress>, Task> installAction)
+    {
+        var infoBar = new InfoBar
+        {
+            Title = await Template.Parse(I18N.InstallingTextTemplate)
+                .RenderAsync(new { action = I18N.Install, name = Id, version = Version }),
+            IsOpen = true,
+            Severity = InfoBarSeverity.Informational,
+            IsClosable = false,
+            Content = BuildProgressContent()
+        };
+        infoBar.Loaded += async (_, _) => { await RunInstallPipelineAsync(infoBar, installAction); };
         var content = await Template.Parse(I18N.InstallTextTemplate)
             .RenderAsync(new { action = I18N.Install, name = Id, version = Version });
-        var dialog = XamlHelper.CreateMessageDialog(I18N.Install, null, null);
-
-        #region TaskProgressGrid
-
-        var taskProgressGrid = new Grid()
-        {
-            Width = 320,
-            ColumnSpacing = 8,
-            Visibility = Visibility.Collapsed,
-            Padding = new Thickness(0, 4, 0, 4)
-        };
-        taskProgressGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
-        taskProgressGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
-        taskProgressGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
-
-        var taskProgressValue = new TextBlock() { Text = "00.00%", VerticalAlignment = VerticalAlignment.Center };
-        var taskProgress = new ProgressBar()
-        {
-            Minimum = 0D,
-            Maximum = 1D,
-            Width = 200,
-            VerticalAlignment = VerticalAlignment.Center,
-            Height = 8
-        };
-        var taskProgressStatus = new TextBlock() { Text = nameof(InstallPipelineStep.Feeding), VerticalAlignment = VerticalAlignment.Center };
-
-        Grid.SetColumn(taskProgressValue, 0);
-        Grid.SetColumn(taskProgress, 1);
-        Grid.SetColumn(taskProgressStatus, 2);
-
-        taskProgressGrid.Children.Add(taskProgressValue);
-        taskProgressGrid.Children.Add(taskProgress);
-        taskProgressGrid.Children.Add(taskProgressStatus);
-
-        #endregion
-
-        #region SubTaskProgressGrid
-
-        var subTaskGrid = new Grid()
-        {
-            Width = 320,
-            Visibility = Visibility.Collapsed,
-            Padding = new Thickness(20, 4, 0, 4),
-            ColumnSpacing = 8
-        };
-        subTaskGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
-        subTaskGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = GridLength.Auto });
-        subTaskGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
-
-        var subTaskProgressValue = new TextBlock()
-        {
-            Text = "00.00%",
-            VerticalAlignment = VerticalAlignment.Center
-        };
-
-        var subTaskProgress = new ProgressBar()
-        {
-            Minimum = 0D,
-            Maximum = 1D,
-            Width = 180,
-            VerticalAlignment = VerticalAlignment.Center,
-            Height = 8
-        };
-
-        var subTaskStatus = new TextBlock()
-        {
-            Text = "--",
-            VerticalAlignment = VerticalAlignment.Center
-        };
-
-        Grid.SetColumn(subTaskProgressValue, 0);
-        Grid.SetColumn(subTaskProgress, 1);
-        Grid.SetColumn(subTaskStatus, 2);
-
-        subTaskGrid.Children.Add(subTaskProgressValue);
-        subTaskGrid.Children.Add(subTaskProgress);
-        subTaskGrid.Children.Add(subTaskStatus);
-
-        #endregion
-
-        dialog.Content = new StackPanel()
-        {
-            Orientation = Orientation.Vertical,
-            Spacing = 4,
-            Children =
-            {
-                new TextBlock()
-                {
-                    Text = content
-                },
-                taskProgressGrid,
-                subTaskGrid
-            }
-        };
-        var progress = new Progress<PipelineProgress>(pipelineProgress =>
-        {
-            taskProgress.Value = pipelineProgress.TotalPercentage;
-            taskProgressValue.Text = pipelineProgress.TotalPercentage.ToString("00.00%");
-            taskProgressStatus.Text = pipelineProgress.Step.ToString();
-
-            subTaskProgress.Value = pipelineProgress.SubPercentage;
-            subTaskProgressValue.Text = pipelineProgress.SubPercentage.ToString("00.00%");
-            subTaskStatus.Text = pipelineProgress.SubStep.ToString();
-            if (Math.Abs(pipelineProgress.TotalPercentage - 1D) < 0.001D)
-            {
-                subTaskGrid.Visibility = Visibility.Collapsed;
-            }
-        });
-
-        dialog.PrimaryButtonText = Sdk.I18n.I18N.Confirm;
-        dialog.IsPrimaryButtonEnabled = true;
-        dialog.PrimaryButtonClick +=
+        await DialogHelper.ShowDialog(XamlHelper.CreateMessageDialog(I18N.Install, content,
             async void (sender, args) =>
             {
                 try
                 {
                     args.Cancel = true;
                     sender.PrimaryButtonText = I18N.Installing;
-                    taskProgressGrid.Visibility = Visibility.Visible;
-                    subTaskGrid.Visibility = Visibility.Visible;
-                    await installAction.Invoke(progress);
+                    DiFactory.Services.Resolve<INotifyService>().NotifyTip(this, infoBar, 0D, TipPopupPosition.Right);
                     sender.Hide();
-                    CheckVersion();
                 }
                 catch (Exception e)
                 {
                     Log.Error(e, "Catch Error in InstallAsync");
                 }
-            };
-        await DialogHelper.ShowDialog(dialog);
+            }
+        ));
     }
+    /// <summary>
+    /// Runs the install pipeline asynchronous.
+    /// </summary>
+    private async Task RunInstallPipelineAsync(InfoBar infoBar, Func<IProgress<PipelineProgress>, Task> installAction)
+    {
+        // 获取 InfoBar 内部控件
+        var taskProgress = infoBar.FindName("TaskProgress") as ProgressBar;
+        var taskProgressValue = infoBar.FindName("TaskProgressValue") as TextBlock;
+        var taskProgressStatus = infoBar.FindName("TaskProgressStatus") as TextBlock;
+
+        var subTaskGrid = infoBar.FindName("SubTaskGrid") as Grid;
+        var subTaskProgress = infoBar.FindName("SubTaskProgress") as ProgressBar;
+        var subTaskProgressValue = infoBar.FindName("SubTaskProgressValue") as TextBlock;
+        var subTaskStatus = infoBar.FindName("SubTaskStatus") as TextBlock;
+
+        var progress = new Progress<PipelineProgress>(p =>
+        {
+            taskProgress?.Value = p.TotalPercentage;
+            taskProgressValue?.Text = p.TotalPercentage.ToString("00.00%");
+            taskProgressStatus?.Text = p.Step.ToString();
+
+            subTaskProgress?.Value = p.SubPercentage;
+            subTaskProgressValue?.Text = p.SubPercentage.ToString("00.00%");
+            subTaskStatus?.Text = p.SubStep.ToString();
+        });
+
+        try
+        {
+            // ⭐ 在 InfoBar Loaded 后执行安装
+            await installAction.Invoke(progress);
+            subTaskGrid?.Visibility = Visibility.Collapsed;
+            // ⭐ 安装成功 → 切换 InfoBar 状态
+            infoBar.Severity = InfoBarSeverity.Success;
+            infoBar.Title = await Template.Parse(I18N.InstallSuccessTextTemplate)
+                .RenderAsync(new { name = Id, version = Version });
+
+            await Task.Delay(4000);
+
+            infoBar.IsOpen = false;
+
+            CheckVersion();
+        }
+        catch (Exception ex)
+        {
+            infoBar.Severity = InfoBarSeverity.Error;
+            infoBar.Title = await Template.Parse(I18N.InstallErrorTextTemplate)
+                .RenderAsync(new { name = Id, version = Version });
+            ;
+            infoBar.Message = ex.Message;
+        }
+    }
+
 
     [RelayCommand(CanExecute = nameof(InstallButtonEnabled))]
     private async Task Install()
@@ -437,7 +440,7 @@ public partial class PluginStoreModel : PluginStoreBaseModel
         if (DownloadUrl == null) return;
         if (InstallStatus == PluginInstallStatus.Install)
         {
-            await ShowInstallContentDialog(InstallPluginFronUrl);
+            await NotifyInfoBar(InstallPluginFronUrl);
         }
         else if (InstallStatus is PluginInstallStatus.Upgrade or PluginInstallStatus.Downgrade)
         {
