7dd2a4c169a991027490df4628a53e8add385e8d
kitUIN <Kit_UIN@outlook.com>
2026-01-23 17:21:47 +0800

    Fix | AddPlugin Type

--- ShadowViewer.Plugin.PluginManager/ViewModels/PluginViewModel.cs
+++ ShadowViewer.Plugin.PluginManager/ViewModels/PluginViewModel.cs
@@ -185,7 +185,7 @@ public partial class PluginViewModel : ObservableObject
     private async Task AddPlugin(XamlRoot root)
     {
         var file = await FilePickerService.PickSingleFileAsync(
-            [".zip", ".rar", ".7z", ".tar"],
+            [".zip", ".rar", ".shad"],
             PickerLocationId.Downloads,
             PickerViewMode.List,
             "ShadowViewer_AddPlugin");
